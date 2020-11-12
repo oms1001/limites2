86d23b057f01144d312d53cca884beca96d35048
oms1001 <oms_100@hotmail.com>
2020-11-11 23:15:51 -0400

    processing

---
+++ puzzle/sketch.js
@@ -0,0 +1,34 @@
+let adri1 = 80;
+let adri2 = 1500;
+let adri3 = 14;
+let adri4 = 300;
+let adri5 = 400;
+let adri6 = 500;
+
+function setup() {
+  createCanvas(displayWidth, displayHeight);
+  adri1 = random(displayHeight)
+  adri2 = random(displayHeight)
+  adri3 = random(displayHeight)
+  adri4 = random(displayHeight)
+  adri5 = random(displayHeight)
+  adri6 = random(displayHeight)
+}
+
+function draw() {
+ 
+  background(0,0,0)
+  fill(255, 230, 0);
+  beginShape();
+  vertex(0, displayHeight/2);
+  vertex(500, displayHeight/4);
+  vertex(mouseX, displayHeight/2)
+  vertex(400, displayHeight/4)
+  vertex(350, displayHeight/2)
+  vertex(mouseX, displayHeight/4)
+  vertex(200, displayHeight/2)
+  vertex(adri1, mouseY)
+  vertex(adri2, displayHeight/2)
+  vertex(displayWidth, displayHeight/4)
+  endShape(CLOSE);
+}
