b7917b2824cea17bb224faf270abe97efc643aa2
oms1001 <oms_100@hotmail.com>
2020-11-13 10:48:56 -0400

    pruebas de audio

---
+++ puzzle/betaundia/beta.js
@@ -0,0 +1,36 @@
+function preload(){
+  sound = loadSound('assets/betaundia.mp3');
+}
+
+function setup(){
+  let cnv = createCanvas(displayWidth/1.5, displayHeight/1.5);
+  cnv.mouseClicked(togglePlay);
+  fft = new p5.FFT();
+  sound.amp(1);
+}
+
+function draw(){
+  background(0);
+
+
+  let waveform = fft.waveform();
+  noFill();
+  beginShape();
+  stroke(255, 230, 0);
+  for (let i = 0; i < waveform.length; i++){
+    let x = map(i, 0, waveform.length, 0, width);
+    let y = map( waveform[i], -1, 1, 0, height);
+    vertex(x,y);
+  }
+  endShape();
+  textSize(10)
+  text('Click para escuchar', 20, 20);
+}
+
+function togglePlay() {
+  if (sound.isPlaying()) {
+    sound.pause();
+  } else {
+    sound.loop();
+  }
+}
